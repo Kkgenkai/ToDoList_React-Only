7910b2f234da18b883a73f76409e27a5184ad2a7
AbeniB <abenischool@gmail.com>
2025-04-07 14:13:27 +0300

    A Little Update on the App.jsx Code (#1)
    * Commit Upto Date
    
    * Commit
    
    * Organize Code
    Organized The Code
    Made It More Functional Than Inline

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,29 @@ export default function App(){
   const [item, setItem] = useState("");
   const [lists, setLists] = useState([]);
 
+  const handleChange = (e) =>{
+    const {value} = e.target; 
+    setItem(value);
+  }
+
+  function handleClick(e){
+    setLists(prevValue => [...prevValue, item]);
+  }
+
+  function list_Items(){
+    return lists.map((e, i) => <li onClick={() => {
+      const nValues = lists.filter((e, index) => i !== index);
+      setLists(nValues);
+    }}>{e}</li>)
+  }
+
+
   return(
     <div className="d-flex flex-direction flex-column mx-5" style={{width: "30%"}}>
       <h1 className="text-center">Todo List</h1>
-      <input type="text" onChange={e => {const {value} = e.target; setItem(value);}} value={item}/>
-      <Button variant="secondary" onClick={(e)=>{setLists(prevValue => [...prevValue, item])}}>Add</Button>
-      {lists.map((e, i) => <li onClick={() => {
-        const nValues = lists.filter((e, index) => i !== index);
-        setLists(nValues);
-      }}>{e}</li>)}
-      
+      <input type="text" onChange={handleChange} value={item}/>
+      <Button variant="secondary" onClick={handleClick}>Add</Button>
+      {list_Items()}
     </div>
   )
 }
