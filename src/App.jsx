884d7bc6a0711b4c11135e02a63e01dba6391d9f
AbeniB <abenischool@gmail.com>
2025-04-07 10:26:38 +0300

    Add Feature "Add Lists" added an "Add" Button for adding Items to the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
 
 export default function App(){
+  const [item, setItem] = useState("");
+  const [lists, setLists] = useState([]);
+
   return(
     <div className="d-flex flex-direction flex-column mx-5" style={{width: "30%"}}>
       <h1 className="text-center">Todo List</h1>
-      <input type="text"/>
-      <Button variant="secondary">Add</Button>
+      <input type="text" onChange={e => {const {value} = e.target; setItem(value);}} value={item}/>
+      <Button variant="secondary" onClick={(e)=>{setLists(prevValue => [...prevValue, item])}}>Add</Button>
+      {lists.map(e => <li>{e}</li>)}
     </div>
   )
 }
