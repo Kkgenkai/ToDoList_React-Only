a9c12dea270a609edbb8ea9a0d7f7a88a65f205d
AbeniB <abenischool@gmail.com>
2025-04-07 10:14:30 +0300

    Remove Button Group Replaced Button Group with Single Button Named: Add

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function App(){
   return(
     <div className="d-flex flex-direction flex-column mx-5" style={{width: "30%"}}>
-      <h1 className="bg-dark">Todo List</h1>
+      <h1 className="text-center">Todo List</h1>
       <input type="text"/>
-      <ButtonGroup aria-label="Basic example">
-        <Button variant="secondary">Left</Button>
-        <Button variant="secondary">Middle</Button>
-        <Button variant="secondary">Right</Button>
-      </ButtonGroup>
+      <Button variant="secondary">Add</Button>
     </div>
   )
 }
