0e56f249b95758206427f0fea1c35a4448278ab7
Abenezer Berhanu <abenischool@gmail.com>
2025-04-08 22:00:41 +0300

    Fix for the Issue #5 and Completing The Milestone (#6)
    * issue_fix: fix no key prop error
    
    * Issue #5 Fix

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,10 @@
-import Item from './Item';
 import { v4 as uuidv4 } from 'uuid';
+import Item from './Item';
 
 export default function List({list, deleteItem}){
     
     function list_Items(){
-        return list.map((e, i) => <Item key={uuidv4()} onClick={()=> {deleteItem(i)}} todo={e}/>)
+        return list.map((e, i) => <Item key={uuidv4()} remove={()=> {deleteItem(i)}} todo={e}/>)
     }
 
     return(
